a48f66e48c201a1d9bf3c2216955d4b4d9124fbb
akofisher <akolekveishvili@gmail.com>
2021-05-12 16:09:38 +0400

    NEW

--- functions.js
+++ functions.js
@@ -1,33 +1,52 @@
-let str = 
-`73167176531330624919225119674426574742355349194934
-96983520312774506326239578318016984801869478851843
-85861560789112949495459501737958331952853208805511
-12540698747158523863050715693290963295227443043557
-66896648950445244523161731856403098711121722383113
-62229893423380308135336276614282806444486645238749
-30358907296290491560440772390713810515859307960866
-70172427121883998797908792274921901699720888093776
-65727333001053367881220235421809751254540594752243
-52584907711670556013604839586446706324415722155397
-53697817977846174064955149290862569321978468622482
-83972241375657056057490261407972968652414535100474
-82166370484403199890008895243450658541227588666881
-16427171479924442928230863465674813919123162824586
-17866458359124566529476545682848912883142607690042
-24219022671055626321111109370544217506941658960408
-07198403850962455444362981230987879927244284909188
-84580156166097919133875499200524063689912560717606
-05886116467109405077541002256983155200055935729725
-71636269561882670428252483600823257530420752963450`;
-str = str.replace(/\n/g, "");
-let max = 0;
-for (let i = 0; i < str.length - 12; i++) {
-  let part = str.substr(i, 4);
-  let currentProduct = 1;
-  for (let n of part) {
-    currentProduct *= +n;
+// Burger NavBar
+
+function firstFunction() {
+   let x = document.getElementById("myLinks");
+   if (x.style.display === "block") {
+    x.style.display = "none";
+   } else {
+    x.style.display = "block";
+   }
   }
-  max = Math.max(max, currentProduct);
+
+// Slider
+
+let slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
 }
 
-console.log(max);
+function showDivs(n) {
+  let i;
+  let x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block"; }
+
+  // Modal
+
+const addModal = document.getElementById('add-modal');
+const startButton = document.querySelector('header button');
+const cancelButton = addModal.querySelector('.btn--passive');
+
+const toggleBackdrop = () => {
+  backdrop.classList.toggle('visible');
+};
+
+const toggleModal = () => { 
+  addModal.classList.toggle('visible');
+  toggleBackdrop();
+};
+
+const cancelAdd = () => {
+  toggleModal();
+};
+
+
+startButton.addEventListener('click', toggleModal);
+cancelButton.addEventListener('click', cancelAdd)
