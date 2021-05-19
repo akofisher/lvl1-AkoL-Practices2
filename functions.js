3bd4ee4a438d062e9e500c3277fd7e722d0ae431
akofisher <akolekveishvili@gmail.com>
2021-05-19 18:43:04 +0400

    NEWW

--- functions.js
+++ functions.js
@@ -5,17 +5,14 @@ let SHOWING = document.getElementById('showing');
 let BACKCALL = document.getElementById('back');
 
 function transition() {
-  SHOWING.style.marginLeft = '-5px';
+  SHOWING.style.marginLeft = '-220px';
   SHOWING.style.transition = '1s';
 }
 
 function backcall() {
-  SHOWING.style.marginLeft = '-300px';
+  SHOWING.style.marginLeft = '-700px';
   SHOWING.style.transition = '1s';
 }
 
 CLICKER.addEventListener('click', transition);
 BACKCALL.addEventListener('click', backcall);
-
-
-
